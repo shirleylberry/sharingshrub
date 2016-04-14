af2a0fa010485715cde3a8411f4a88bb0c4564f6
shirleylberry <shirleylberry@gmail.com>
2016-04-14 14:25:08 -0400

    mailer action in pledges controller'

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,30 +1,29 @@
 class PledgesController < ApplicationController
+  def new
+      @event = set_event
+      @pledge = @event.pledges.build
+  end
 
-    def new
-        @event = set_event
-        @pledge = @event.pledges.build
+  def create
+    @pledge = Pledge.new(pledge_params)
+    @pledge.donor = Donor.find_or_create_by(user: current_user)
+    @pledge.event = set_event
+    if @pledge.save
+        @pledge.event.update_funded_status_if_goal_reached
+        redirect_to event_path(set_event)
+    else
+        render 'new'
     end
+  end
 
-    def create
-        @pledge = Pledge.new(pledge_params)
-        @pledge.donor = Donor.find_or_create_by(user: current_user)
-        @pledge.event = set_event
-        if @pledge.save
-            @pledge.event.update_funded_status_if_goal_reached
-            redirect_to event_path(set_event)
-        else
-            render 'new'
-        end
-    end
+  private
 
-    private
+  def pledge_params
+      params.require(:pledge).permit(:amount)
+  end
 
-    def pledge_params
-        params.require(:pledge).permit(:amount)
-    end
-
-    def set_event
-       Event.find(params[:event_id])
-    end
+  def set_event
+     Event.find(params[:event_id])
+  end
 
 end
---
+++ app/mailers/pledge_mailer.rb
@@ -0,0 +1,8 @@
+class PledgeMailer < ApplicationMailer
+  def pledge_creation_email(pledge)
+    @user = pledge.donor.user
+    @event = pledge.event
+    # @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Thanks for Donating to ' + @event.title + '!')
+  end
+end
