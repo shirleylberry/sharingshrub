5dc7e6cde18332a49df94b5e2b802b19ea3d2475
shirleylberry <shirleylberry@gmail.com>
2016-04-18 10:56:03 -0400

    working rake task to create charities and causes from charitywatch

--- app/models/adapters/charity_client.rb
+++ app/models/adapters/charity_client.rb
@@ -1,8 +1,14 @@
 module Adapters
   class CharityClient
     def create_charities(charity_hashes)
-      # cause_names.map{|name| Cause.find_or_create_by(name: name)}
-      binding.pry
+      charity_hashes.each do |hash|
+        hash.delete(:url_path)
+        cause_name = hash.delete(:cause)
+        cause = Cause.find_or_create_by(name: cause_name)
+        c = Charity.create(hash)
+        c.causes.push(cause)
+        # binding.pry
+      end
     end
   end
 end
--- app/models/adapters/charity_watch_connection.rb
+++ app/models/adapters/charity_watch_connection.rb
@@ -14,7 +14,7 @@ module Adapters
 
     def titleize(name)
       words = name.split(/[\s,-]/)
-      new_name = words.map{|word| word[0] + word.slice(1..-1).downcase}.join(" ")
+      new_name = words.map{|word| word[0] + word.slice(1..-1).downcase unless word.empty? }.join(" ")
     end
 
     def get_cause_names
@@ -26,6 +26,7 @@ module Adapters
     end
 
     def get_charities
+      @charities = []
       doc = Nokogiri::HTML(open(@url))
       results_div = doc.css('div#results')
       @current_category = ""
@@ -38,7 +39,9 @@ module Adapters
         else
           puts el.attr("class")
         end
+        # puts el.text, @charities
       end
+      @charities
     end
 
     def charity_from_row(row)
@@ -46,15 +49,28 @@ module Adapters
       charity_name = row.css('td').first.text
       rating = row.css('td')[1].text
       url_path = row.css('a').attr("href").value
-      char_hash = {charity_name: charity_name, rating: rating, url_path: url_path, category: @current_category}
-      binding.pry
+      char_hash = {name: charity_name, rating: rating, url_path: url_path, category: @current_category}
     end
 
     def create_charity_hashes(table)
-      # binding.pry
       table.children.children.each do |row|
-        charity_from_row(row)
+        hash = charity_from_row(row)
+        new_hash = specific_charity_data(hash)
+        @charities.push(new_hash)
       end
+    end
+
+    def specific_charity_data(charity_hash)
+      @charity_url = "https://www.charitywatch.org" + charity_hash[:url_path]
+      doc = Nokogiri::HTML(open(@charity_url))
+      c_info_div = doc.at_css('.charity_row .charity_section').children
+      address = [c_info_div[5], c_info_div[7], c_info_div[9]].join(" ")
+      website = c_info_div[11].attr("href")
+      mission = doc.css('.charity_row .charity_section')[3].text.gsub(/[\t\n]/, "").gsub("Stated Mission ", "")
+      charity_hash[:website] = website
+      charity_hash[:address] = address
+      charity_hash[:mission] = mission
+      charity_hash
     end
   end
 end
